6572315420d36e4555b9ac3346cc1d3302b5464a
Renan.Nascimento <renan.nascimento@sou.ufac.br>
2026-05-28 19:26:00 -0500

    atividade de if

---
+++ teste.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class teste {
+    public static void main(String [] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double n1 = sc.nextDouble();
+        double n2 = sc.nextDouble();
+        double media = (n1 + n2)/2;
+
+        if (media >= 7){
+            System.out.println("O ALUNO PASSOU COM MÉDIA: "+ media);
+        } else {
+            System.out.println("O ALUNO NÃO PASSOU COM MÉDIA");
+        }
+    }
+}
