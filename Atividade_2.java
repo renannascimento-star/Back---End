aec43eb3632fe4b56972504824f8d984e8b45337
Renan.Nascimento <renan.nascimento@sou.ufac.br>
2026-05-28 19:47:21 -0500

    atividade com while

---
+++ Atividade_2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Atividade_2 {
+    public static void main(String[] args) {
+        int contador = 0;
+        while (contador < 3) {
+            System.out.println("Olá");
+            contador++;
+        }
+    }
+}
